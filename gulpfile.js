e19a04a6e5d3ad785caa5c9bf902110daae0434f
Taiki Shiraishi <tiki@NoOneLivesForever.local>
2016-03-29 22:18:54 +0900

    Common dictionary add

--- gulpfile.js
+++ gulpfile.js
@@ -9,13 +9,16 @@ gulp.task('json-html', function() {
 
     var json = JSON.parse(fs.readFileSync(json_file)); // JSONの読み込み
     var pages = json.pages;
+    var common = json.common;
+    console.log(common);
 
     for (var i = 0; i < pages.length; i++) { // ページの数だけループ
         var id = pages[i].id;
 
         gulp.src(tmp_file)
             .pipe(ejs({
-                jsonData: pages[i] // JSONのデータをejsに渡す
+                  jsonData: pages[i], // JSONのデータをejsに渡す
+                commonData: common // JSONのデータをejsに渡す
             }))
             .pipe(rename(id + ".html")) // (id).htmlにファイル名を変更
             .pipe(gulp.dest("dist")); // 指定したフォルダに出力
